1bc5f57092023023ca1e4c4416fc4db520ef8366
cb218 <cb218@students.uwf.edu>
2022-08-14 15:18:09 -0500

    daemon and setup-script tested on ubuntu

--- setup-service.sh
+++ setup-service.sh
@@ -1,10 +1,24 @@
 #! /bin/bash
+# This script will set up (and re-set-up) the daemon for the testbed-monitor
+
+# build the main program
 go build main.go
+
+# copy all the necessary program files for the daemon
 sudo rm -R /usr/lib/monitor
 sudo mkdir /usr/lib/monitor
 sudo cp testbed-monitor.service /usr/lib/monitor/testbed-monitor.service
 sudo cp configuration.yaml /usr/lib/monitor/configuration.yaml
 sudo cp .env /usr/lib/monitor/.env
 sudo cp main /usr/lib/monitor/main
+
+# copy the service to the
+sudo rm /etc/systemd/system/testbed-monitor.service
 sudo cp testbed-monitor.service /etc/systemd/system/testbed-monitor.service
 sudo systemctl enable testbed-monitor
+
+# kill previously running programs and start
+sudo pkill main
+sudo systemctl start testbed-monitor
+
+# view daemon status at anytime with >sudo systemctl status testbed-monitor
