5b86f56076263451a13efe12561c32ebc13efe85
cb218 <cb218@students.uwf.edu>
2022-09-13 17:04:08 -0500

    combined daily&weekly aggregate funcs

--- report/aggregate.go
+++ report/aggregate.go
@@ -17,6 +17,7 @@ type Aggregate struct {
 	aggregateHour   int
 	apiIP           string
 	apiPort         string
+	thresholds      map[string]trigger
 }
 
 type reportData struct {
@@ -41,7 +42,7 @@ type reportTemplate struct {
 type trigger struct {
 	field    string
 	operator string
-	trigger  int
+	trigger  int64
 }
 
 func NewAggregate(statusChan chan *StatusReport, aggregatePeriod int, aggregateHour int, apiIP string, apiPort string) *Aggregate {
@@ -51,6 +52,7 @@ func NewAggregate(statusChan chan *StatusReport, aggregatePeriod int, aggregateH
 	aggregate.aggregateHour = aggregateHour
 	aggregate.apiIP = apiIP
 	aggregate.apiPort = apiPort
+	aggregate.thresholds = make(map[string]trigger)
 	return aggregate
 }
 
@@ -62,6 +64,7 @@ var unreachableFlag bool
 func (aggregate *Aggregate) Start(iPs *[]string) {
 	dailyTicker := time.NewTicker(time.Duration(aggregate.aggregatePeriod) * time.Minute)
 	reportAggregate := make(map[time.Time]*StatusReport)
+	fields := reflect.VisibleFields(reflect.TypeOf(struct{ reportData }{}))
 	var i int
 	for {
 		select {
@@ -71,16 +74,16 @@ func (aggregate *Aggregate) Start(iPs *[]string) {
 				unreachableFlag = false
 				if time.Now().Weekday().String() == "Sunday" { // Weekly report on Sundays
 					for i = 0; i < len(*iPs); i++ {
-						weeklyAggregator(reportAggregate, (*iPs)[i], &aggregatedReport)
+						aggregator(reportAggregate, (*iPs)[i], &aggregatedReport, "Week")
 						emailData.report = append(emailData.report, aggregatedReport)
 					}
 					emailData.reportType = "Weekly"
 				} else { // Daily report
 					for i = 0; i < len(*iPs); i++ {
-						dailyAggregator(reportAggregate, (*iPs)[i], &aggregatedReport)
+						aggregator(reportAggregate, (*iPs)[i], &aggregatedReport, "Day")
 						emailData.report = append(emailData.report, aggregatedReport)
 					}
-					emailData.reportType = "Daily"
+					emailData.reportType = "24 Hour"
 				}
 				emailData.timestamp = time.Now().Format("Jan 02 2006 15:04:05")
 				subject = emailData.reportType + " Testbed Status Report " + emailData.timestamp
@@ -95,16 +98,27 @@ func (aggregate *Aggregate) Start(iPs *[]string) {
 			if !msg.Reachable {
 				aggregate.towerAlertInApp(msg.TowerIP)
 			}
-			//for _, trigger := range aggregate.threshold {
-			//	log.Println(trigger)
-			//	// if field operator trigger {}
-			//}
+			for thresholdField := range aggregate.thresholds {
+				for _, reportField := range fields {
+					if aggregate.thresholds[thresholdField].field == reportField.Name {
+						if aggregate.thresholds[thresholdField].operator == ">" {
+							log.Println(aggregate.thresholds[thresholdField].field)
+							log.Println(reportField.Name)
+							log.Println(getReportValue(msg, reportField.Name))
+							if getReportValue(msg, reportField.Name) > aggregate.thresholds[thresholdField].trigger {
+								log.Println("greater than!!!!!!")
+							}
+						}
+						// less than
+						// equal to
+					}
+				}
+			}
 		}
 	}
 }
 
 func (aggregate *Aggregate) SetTriggers(threshold []thresholds.Config) {
-	thresholds := make(map[string]trigger)
 	fields := reflect.VisibleFields(reflect.TypeOf(struct{ reportData }{}))
 	log.Println("Thresholds:")
 	for _, field := range fields {
@@ -113,8 +127,8 @@ func (aggregate *Aggregate) SetTriggers(threshold []thresholds.Config) {
 				setTrigger := trigger{}
 				setTrigger.field = t.Field
 				setTrigger.operator = t.Operator
-				setTrigger.trigger, _ = strconv.Atoi(t.Trigger)
-				thresholds[field.Name] = setTrigger
+				setTrigger.trigger, _ = strconv.ParseInt(t.Trigger, 10, 64)
+				aggregate.thresholds[field.Name] = setTrigger
 				log.Printf("%s %s %s\n", t.Field, t.Operator, t.Trigger)
 			}
 		}
@@ -142,74 +156,20 @@ func (aggregate *Aggregate) towerAlertInApp(alertIP string) {
 	}
 }
 
-func dailyAggregator(reports map[time.Time]*StatusReport, iP string, templateData *reportData) {
+func aggregator(reports map[time.Time]*StatusReport, iP string, templateData *reportData, reportType string) {
 	var usedRAMAvg, ramCounter, usedDiskAvg, diskCounter, cpuAvg, cpuCounter, rebootCounter int64 = 0, 0, 0, 0, 0, 0, 0
 	var totalRAM, totalDisk, maxTemp int64 = 0, 0, 0
-	compareTime := time.Time{}
-	templateData.tower = iP
-	for key, element := range reports {
-		if key.Day() == time.Now().Day() && element.TowerIP == iP {
-			if element.Timestamp.AsTime().After(compareTime) {
-				templateData.reachable = true
-				if element.Reachable == true {
-					templateData.arduinoReached = element.LastArduinoReachableTimestamp
-					templateData.towerReached = element.LastTowerReachableTimestamp
-					templateData.bootTime = element.BootTimestamp
-				}
-				if totalRAM == 0 {
-					totalRAM = element.RAMTotal
-				}
-				if totalDisk == 0 {
-					totalDisk = element.DiskTotal
-				}
-				if !element.Reachable {
-					templateData.reachable = false
-				}
-				compareTime = element.Timestamp.AsTime()
-			}
-		}
-		rebootCounter = rebootCounter + element.RebootsCurrentDay
-		usedRAMAvg = usedRAMAvg + element.RAMUsed
-		ramCounter++
-		usedDiskAvg = usedDiskAvg + element.DiskUsed
-		diskCounter++
-		cpuAvg = cpuAvg + element.CPUAvg
-		cpuCounter++
-		if element.Timestamp.AsTime().Day() == time.Now().Day() && element.TowerIP == iP {
-			if element.Temperature > 0 {
-				if element.Temperature > maxTemp {
-					maxTemp = element.Temperature
-				}
-			}
-		}
-	}
-	if ramCounter > 0 {
-		usedRAMAvg = usedRAMAvg / ramCounter
-	}
-	templateData.usedRAM = strconv.FormatInt(usedRAMAvg, 10) + "/" + strconv.FormatInt(totalRAM, 10) + " MB"
-	if diskCounter > 0 {
-		usedDiskAvg = usedDiskAvg / diskCounter
-	}
-	templateData.usedDisk = strconv.FormatInt(usedDiskAvg, 10) + "/" + strconv.FormatInt(totalDisk, 10) + " GB"
-	if cpuCounter > 0 {
-		cpuAvg = cpuAvg / cpuCounter
-	}
-	templateData.cpu = strconv.FormatInt(cpuAvg, 10) + "%"
-	templateData.reboots = strconv.FormatInt(rebootCounter, 10)
-	if templateData.reachable == false {
-		unreachableFlag = true
-	}
-
-}
-
-func weeklyAggregator(reports map[time.Time]*StatusReport, iP string, templateData *reportData) {
-	var usedRAMAvg, ramCounter, usedDiskAvg, diskCounter, cpuAvg, cpuCounter, rebootCounter int64 = 0, 0, 0, 0, 0, 0, 0
-	var totalRAM, totalDisk, maxTemp int64 = 0, 0, 0
+	var interval time.Duration
 	compareTime := time.Time{}
 	templateData.tower = iP
 	for key, element := range reports {
 		templateData.tower = iP
-		if key.After(time.Now().Add(-7*24*time.Hour)) && element.TowerIP == iP {
+		if reportType == "Week" {
+			interval = -7 * 24 * time.Hour
+		} else {
+			interval = -24 * time.Hour
+		}
+		if key.After(time.Now().Add(interval)) && element.TowerIP == iP {
 			if element.Timestamp.AsTime().After(compareTime) {
 				templateData.reachable = true
 				if element.Reachable == true {
@@ -235,7 +195,7 @@ func weeklyAggregator(reports map[time.Time]*StatusReport, iP string, templateDa
 			diskCounter++
 			cpuAvg = cpuAvg + element.CPUAvg
 			cpuCounter++
-			if element.Timestamp.AsTime().After(time.Now().Add(-7*24*time.Hour)) && element.TowerIP == iP {
+			if element.Timestamp.AsTime().After(time.Now().Add(interval)) && element.TowerIP == iP {
 				if element.Temperature > 0 {
 					if element.Temperature > maxTemp {
 						maxTemp = element.Temperature
@@ -262,4 +222,27 @@ func weeklyAggregator(reports map[time.Time]*StatusReport, iP string, templateDa
 		unreachableFlag = true
 	}
 	templateData.temperature = strconv.FormatInt(maxTemp, 10)
+}
+
+func checkGreater(reportVal int64, triggerVal int64) bool {
+	if reportVal > triggerVal {
+		return true
+	} else {
+		return false
+	}
+}
+
+func getReportValue(msg *StatusReport, field string) int64 {
+	//defer func() {
+	//	if r := recover(); r != nil {
+	//		log.Println("Recovered in f", r)
+	//	}
+	//}()
+	//r := reflect.ValueOf(msg)
+	//log.Println(r)
+	//f := reflect.Indirect(r).FieldByName(field)
+	//log.Println(f)
+	return 0 //f.Int()
+
+	//panic: reflect: call of reflect.Value.Int on zero Value
 }
