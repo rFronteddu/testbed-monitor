373c6ccc88fdca72d73fb781bd66245dba49959c
cb218 <cb218@students.uwf.edu>
2022-11-01 20:31:13 -0500

    modified service setup script

--- setup-service.sh
+++ setup-service.sh
@@ -11,8 +11,8 @@ sudo cp testbed-monitor.service /usr/lib/monitor/testbed-monitor.service
 sudo cp configuration.yaml /usr/lib/monitor/configuration.yaml
 sudo cp .env /usr/lib/monitor/.env
 sudo cp traps.json /usr/lib/monitor/traps.json
-sudo cp template.html /usr/lib/monitor/report_template.html
-sudo cp template.html /usr/lib/monitor/notification_template.html
+sudo cp report_template.html /usr/lib/monitor/report_template.html
+sudo cp notification_template.html /usr/lib/monitor/notification_template.html
 sudo cp main /usr/lib/monitor/main
 
 # copy the service to the
